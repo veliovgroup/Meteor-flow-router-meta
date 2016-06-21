0ca23c58e4751f10f7947de4a44c9d3bb08662a7
Dmitriy A. Golev <ceo@veliov.com>
2016-06-21 18:46:29 +0300

    v1.1.1
    - Compatibility with METEOR@1.3.3.1
    - Update package dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:flow-router-meta',
-  version: '1.1.0',
+  version: '1.1.1',
   summary: 'Change meta tags, links to styles (CSS) and scripts on the fly within flow-router',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-meta',
   documentation: 'README.md'
@@ -8,8 +8,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.3');
-  api.use(['underscore', 'coffeescript', 'reactive-var', 'ostrio:flow-router-extra@2.11.0'], 'client');
+  api.use(['underscore', 'coffeescript', 'reactive-var', 'ostrio:flow-router-extra@2.12.2'], 'client');
   api.addFiles('flow-router-meta.coffee', 'client');
   api.export('FlowRouterMeta', 'client');
-  api.imply('ostrio:flow-router-title@2.1.0', 'client');
+  api.imply('ostrio:flow-router-title@2.1.1', 'client');
 });
