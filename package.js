e77f253cc1974fff08a609cb7112ae964c45e6cd
dr.dimitru <ceo@veliov.com>
2017-05-18 08:15:35 +0300

    v2.0.5
     - Compatibility with `meteor@1.4.4.2`
     - Dependencies update
     - Better tests
     - *Regular update*

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:flow-router-meta',
-  version: '2.0.4',
+  version: '2.0.5',
   summary: 'Change meta tags, links to styles (CSS) and scripts on the fly within flow-router',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-meta',
   documentation: 'README.md'
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.4');
-  api.use(['underscore', 'ecmascript', 'ostrio:flow-router-title@3.0.1', 'ostrio:flow-router-extra@3.1.0'], 'client');
+  api.use(['underscore', 'ecmascript', 'ostrio:flow-router-title@3.0.2', 'ostrio:flow-router-extra@3.1.1'], 'client');
   api.mainModule('flow-router-meta.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'ecmascript', 'ostrio:flow-router-extra', 'ostrio:flow-router-meta', 'random'], 'client');
+  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra', 'ostrio:flow-router-meta'], 'client');
   api.addFiles('tests.js', 'client');
 });
