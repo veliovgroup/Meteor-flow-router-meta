70946a293b2d6c6f91a920dc49e13f6605dd2358
dr.dimitru <ceo@veliov.com>
2019-03-25 00:30:38 -0700

    v2.0.15
     - 📦 Update NPM and Atmosphere dependencies
     - 📋 Readme linting and minor update
     - 🤝 Compatibility with `meteor@1.8.0.2`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:flow-router-meta',
-  version: '2.0.14',
+  version: '2.0.15',
   summary: 'Change meta tags, links to styles (CSS) and scripts on the fly within flow-router',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-meta',
   documentation: 'README.md'
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.4');
-  api.use(['ecmascript', 'ostrio:flow-router-title@3.1.3'], 'client');
+  api.use(['ecmascript', 'ostrio:flow-router-title@3.1.4'], 'client');
   api.mainModule('flow-router-meta.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.5.1', 'ostrio:flow-router-meta'], 'client');
+  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.6.2', 'ostrio:flow-router-meta'], 'client');
   api.addFiles('tests.js', 'client');
 });
