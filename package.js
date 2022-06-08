cedef5ff2ca47bca16028e4d9d8df9b0f6d94dbf
dr.dimitru <ceo@veliov.com>
2022-06-08 15:57:00 +0300

    📦 v2.1.2
    *This is maintenance release*
    
    - 🤝 Compatibility with `meteor@2.7.3`
    
    __Dependencies:__
    
    - 📦 `ostrio:flow-router-extra@3.8.0`, *was `v3.7.4`*
    - 📦 `ostrio:flow-router-title@3.2.2`, *was `v3.2.1`*

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'ostrio:flow-router-meta',
-  version: '2.1.1',
-  summary: 'Change meta tags, links to styles (CSS) and scripts on the fly within flow-router',
+  version: '2.1.2',
+  summary: 'Change meta tags, links to styles (CSS) and scripts on the fly via flow-router definition',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-meta',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
   api.versionsFrom('METEOR@1.4');
-  api.use(['ecmascript', 'ostrio:flow-router-title@3.2.1'], 'client');
+  api.use(['ecmascript', 'ostrio:flow-router-title@3.2.2'], 'client');
   api.mainModule('flow-router-meta.js', 'client');
 });
 
 Package.onTest((api) => {
-  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.7.4', 'ostrio:flow-router-title@3.2.1'], 'client');
+  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.8.0', 'ostrio:flow-router-title@3.2.2'], 'client');
   api.addFiles('tests.js', 'client');
 });
