f2a2767ebc45ccbcf26c2e094e6a7876d02609e8
dr.dimitru <ceo@veliov.com>
2025-09-24 13:42:31 +0300

    📦 v2.3.1
    - 🤝 Compatibility with latest `meteor@3.3.2`
    - 🤝 Compatibility with `ostrio:flow-router-extra@3.12.1`
    - 🤝 Compatibility with `ostrio:flow-router-title@3.4.1`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:flow-router-meta',
-  version: '2.3.0',
+  version: '2.3.1',
   summary: 'Set and update meta, link, and script tags via flow-router hooks',
   git: 'https://github.com/veliovgroup/Meteor-flow-router-meta',
   documentation: 'README.md'
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse((api) => {
   api.versionsFrom(['1.4', '2.8.0', '3.0.1']);
-  api.use(['ecmascript', 'ostrio:flow-router-title@3.4.0'], 'client');
+  api.use(['ecmascript', 'ostrio:flow-router-title@3.4.1'], 'client');
   api.mainModule('flow-router-meta.js', 'client');
 });
 
 Package.onTest((api) => {
-  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.12.0', 'ostrio:flow-router-title@3.4.0'], 'client');
+  api.use(['tinytest', 'ecmascript', 'jquery', 'random', 'ostrio:flow-router-extra@3.12.1', 'ostrio:flow-router-title@3.4.1'], 'client');
   api.addFiles('tests.js', 'client');
 });
